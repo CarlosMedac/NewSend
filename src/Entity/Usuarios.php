e6df77202adc5c0c13a1961bd730fe99d931273d
CarlosMedac <cml0006@alu.medac.es>
2022-05-02 10:42:42 +0200

    primer commit

---
+++ src/Entity/Usuarios.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsuariosRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UsuariosRepository::class)
+ */
+class Usuarios
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Nombre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Contrasenia;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->Nombre;
+    }
+
+    public function setNombre(string $Nombre): self
+    {
+        $this->Nombre = $Nombre;
+
+        return $this;
+    }
+
+    public function getContrasenia(): ?string
+    {
+        return $this->Contrasenia;
+    }
+
+    public function setContrasenia(string $Contrasenia): self
+    {
+        $this->Contrasenia = $Contrasenia;
+
+        return $this;
+    }
+}
