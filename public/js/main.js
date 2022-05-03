552d60a66fe0247798393913ae0d8f9c65a5a371
CarlosMedac <cml0006@alu.medac.es>
2022-05-03 17:37:07 +0200

    estilos home

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,1 @@
 
-$(document).ready(function(){
-    $(".enlace").mouseenter(function(){
-        $(this).children(".enl_nom").animate({
-            width: 'toggle'
-        });
-    });
-    $(".enlace").mouseleave(function(){
-        $(this).children(".enl_nom").animate({
-            width: 'toggle'
-        });
-      });
-  });
