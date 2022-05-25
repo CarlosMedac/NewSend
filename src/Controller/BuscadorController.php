36aa2af179e3971c9f924d540e8e487be64651d9
EnmaMP <enma97mp@gmail.com>
2022-05-25 14:27:13 +0200

    Buscador fase alpha

--- src/Controller/BuscadorController.php
+++ src/Controller/BuscadorController.php
@@ -19,65 +19,55 @@ use Doctrine\ORM\Query;
 class BuscadorController extends AbstractController
 {
     /**
-     * @Route("/buscador", name="app_buscador")
+     * @Route("/buscador", name="buscador")
      */
  
-    public function index(Request $request): Response
+    public function index(Request $request,ManagerRegistry $doctrine): Response
     {
-        $userEncontrado="";
-        $user=new User();
-        $form=$this->createForm(BarraBusquedaType::class, $user);
+       /** @var User $user */
+        $user = $this->getUser();
+        if(!empty($user)){
+            $userId = $user->getId();
+            $username = $user->getUserIdentifier();
+            }
+        $form=$this->createForm(BarraBusquedaType::class);
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid())
-        {
-            $entityManager = $this->getDoctrine()->getManager();
-            $query = new Query($entityManager);
+        
+        if ($request->isXmlHttpRequest()) {
+            try {
+                $entityManager = $doctrine->getManager();
+                $query = new Query($entityManager);
+                $requestUser= $request->get('userSolicitado');
+                $query->setDQL(
+                    "SELECT u.username
+                    FROM App\Entity\User u 
+                    WHERE u.username LIKE :usrname"
+                )
+                ->setParameter('usrname',"%".$requestUser."%" );
+                
+                $traerUsers = $query->getResult();
 
-         
-            $userEncontrado="a";
-
-            $requestUser= $request->get('username');
-            $query->setDQL(
-                "SELECT u.username
-                FROM App\Entity\User u 
-                WHERE u.username LIKE :usrname"
-            )
-            ->setParameter('usrname',"%".$userEncontrado."%" );
-
-            
-            $verUsers = $query->getResult();
-
-            $users = $this->render('buscador/index.html.twig', array(
-                'test' => $verUsers,
-                'formulario'=>$form->createView()
-            ));
-            
-            return $users;
-        }
-        else{
-             return $this->render('buscador/index.html.twig', [
-            'userEncontrado' => $userEncontrado,
+                $usersBD = $this->render('buscador/barraBusqueda.html.twig', array(
+                                'test' => $traerUsers,
+                                'usuarioLogueadoId' => $userId,
+                            ));
+                return ($usersBD);
+            } 
+            catch (Exception $e)
+            {
+                return new JsonResponse(['ERROR...'=>$e]);
+            }
+        } 
+        else {
+           return $this->render('buscador/index.html.twig', [
+            'pagina' => 'Buscador',
             'formulario'=>$form->createView(),
-            'test' => "",
+            'usuarioLogueadoId' => $userId,
+            'test' => " ",
         ]);
         }
-
-       
-        
     }
-    // public function buscador(ManagerRegistry $doctrine, $username){
-         
-    //     $em = $doctrine->getManager();
-    //     $buscarUsuarios = $em->getRepository(User::class);
-    //     $userEncontrado= $buscarUsuarios->find($username);
-    //     $form=$this->createFormBuilder( null)
-    //     ->add('query', TextType::class)
-    //     ->add('buscar',SubmitType::class)
-    //     ->getForm();
-    //     return $this->render('buscador/barraBusqueda.html.twig',[
-    //         'form'=>$form->createView()
-    //     ]);
-    // }
+ 
 
 /**
      * @Route("/buscadorUsers", name="buscadorUsers")
@@ -97,13 +87,26 @@ class BuscadorController extends AbstractController
                 ->setParameter('usrname',"%".$requestUser."%" );
                 
                 $traerUsers = $query->getResult();
-                return new JsonResponse(['Users Encontrados'=>$traerUsers]);
+
+            //   $this->render('buscador/index.html.twig', array(
+            //         'test' => $traerUsers,
+                     
+            //     ));
+            //     return new JsonResponse(['usersEncontrados'=>$traerUsers]);
+
+                $mensajes = $this->render('buscador/index.html.twig', array(
+                    'test' => $traerUsers,
+          
+                ));
+                
+                return ($mensajes);
             } 
-            catch (Exception $e){
+            catch (Exception $e)
+            {
                 return new JsonResponse(['ERROR'=>$e]);
             }
-            
-        } else {
+        } 
+        else {
             return new JsonResponse(['ELSE'=>"Terrible"]);
         }
     }
