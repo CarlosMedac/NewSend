f227a6be931d6a409de8b0b261425352f72c7051
CarlosMedac <cml0006@alu.medac.es>
2022-05-26 23:28:03 +0200

    cambios css manias y conflictos del merge

--- public/js/buscador.js
+++ public/js/buscador.js
@@ -11,8 +11,6 @@ $( "#formBUSQUEDA" ).keyup(function() {
             url: "/buscador",
             data: {"userSolicitado":textoBuscador},
             success: function (data) {
-                console.log( data );
-                console.log("Ajax Success :)"); 
                 if(data !=""){
                     $("#usuariosEncontrados").html("");
                 
@@ -38,16 +36,7 @@ $( "#formBUSQUEDA" ).keyup(function() {
   function visitarPerfil(idPerfil) {
     window.location.href = "/perfil/"+idPerfil;
 }
-<<<<<<< HEAD
-// function DejarSeguir(userLogued,idseguir) {
-//     $.ajax({
-//         type: 'POST',
-//         url: "/unfollow",
-//         data: ({userLogued: userLogued,idseguir: idseguir}),
-//         async: true,
-//         dataType: "json",    
-//         beforeSend: function ( xhr ) {
-=======
+
 function DejarSeguirBuscador(userLogued,idseguir) {
     $.ajax({
         type: 'POST',
@@ -56,54 +45,32 @@ function DejarSeguirBuscador(userLogued,idseguir) {
         async: true,
         dataType: "json",    
         beforeSend: function ( xhr ) {
->>>>>>> 5c8aeebd7f73989d2084993cf0e051fcc9561df4
-            
-//             $('[name='+idseguir+']').html('<img src="../../uploads/img/ajax-loader.gif" id="ani_img_seguir"/>');
-            
-//          },
-//         success: function (data) {
-//             var textoBuscador= $("#barra_busqueda_username").val();
-            
-//         $.ajax({
-//             type: 'POST',
-//             url: "/buscador",
-//             data: {"userSolicitado":textoBuscador},
-//             success: function (data) {
-//                 console.log( data );
-//                 console.log("Ajax Success :)"); 
-//                 if(data !=""){
-//                     $("#usuariosEncontrados").html("");
-                
-//                 }         
-//                 $('#usuariosEncontrados').append(data);
-
-
-//             },
-//              error: function (xhr,responseText, ajaxOptions, thrownError) {
-//                 console.log(xhr.responseText);
-//                 console.log(thrownError);
-//                 console.log(ajaxOptions);
-//               }
+            $('[name='+idseguir+']').html('<img src="../../uploads/img/ajax-loader.gif" id="ani_img_seguir"/>'); 
+         },
+        success: function (data) {
+            var textoBuscador= $("#barra_busqueda_username").val();      
+        $.ajax({
+            type: 'POST',
+            url: "/buscador",
+            data: {"userSolicitado":textoBuscador},
+            success: function (data) {
+                if(data !=""){
+                    $("#usuariosEncontrados").html("");          
+                }         
+                $('#usuariosEncontrados').append(data);
+            },
+             error: function (xhr,responseText, ajaxOptions, thrownError) {
+                console.log(xhr.responseText);
+                console.log(thrownError);
+                console.log(ajaxOptions);
+              }
           
-//         });
-//         }
-//     });
-// }
+        });
+        }
+    });
+}
 
-<<<<<<< HEAD
-// function Seguir(userLogued,idseguir) {
-//     $.ajax({
-//         type: 'POST',
-//         url: "/follow",
-//         data: ({userLogued: userLogued,idseguir: idseguir}),
-//         async: true,
-//         dataType: "json",
-//         beforeSend: function ( xhr ) {
-//             $('[name='+idseguir+']').html('<img src="../../uploads/img/ajax-loader.gif" id="ani_img_seguir"/>');
-//          },
-//         success: function (data) {
-//             var textoBuscador= $("#barra_busqueda_username").val();
-=======
+
 function SeguirBuscador(userLogued,idseguir) {
     $.ajax({
         type: 'POST',
@@ -116,31 +83,24 @@ function SeguirBuscador(userLogued,idseguir) {
          },
         success: function (data) {
             var textoBuscador= $("#barra_busqueda_username").val();
->>>>>>> 5c8aeebd7f73989d2084993cf0e051fcc9561df4
-
-//         $.ajax({
-//             type: 'POST',
-//             url: "/buscador",
-//             data: {"userSolicitado":textoBuscador},
-//             success: function (data) {
-//                 console.log( data );
-//                 console.log("Ajax Success :)"); 
-//                 if(data !=""){
-//                     $("#usuariosEncontrados").html("");
-                
-//                 }         
-//                 $('#usuariosEncontrados').append(data);
-
-
-//             },
-//              error: function (xhr,responseText, ajaxOptions, thrownError) {
-//                 console.log(xhr.responseText);
-//                 console.log(thrownError);
-//                 console.log(ajaxOptions);
-//               }
+        $.ajax({
+            type: 'POST',
+            url: "/buscador",
+            data: {"userSolicitado":textoBuscador},
+            success: function (data) {
+                if(data !=""){
+                    $("#usuariosEncontrados").html("");            
+                }         
+                $('#usuariosEncontrados').append(data);
+            },
+             error: function (xhr,responseText, ajaxOptions, thrownError) {
+                console.log(xhr.responseText);
+                console.log(thrownError);
+                console.log(ajaxOptions);
+              }
           
-//         });
+        });
     
-//         }
-//     });
-// }
+        }
+    });
+}
